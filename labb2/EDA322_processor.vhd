a298d6e165e1bc4bb98160052e6177fb03580cb4
Kasper Fång Wiiik <kasperfa@student.chalmers.se>
2024-02-10 14:45:38 +0100

    Fixade_ALU

--- labb2/EDA322_processor.vhd
+++ labb2/EDA322_processor.vhd
@@ -136,7 +136,8 @@ SIGNAL flagLd:std_logic;
 SIGNAL C:std_logic;
 SIGNAL E:std_logic;
 SIGNAL Z:std_logic;
-SIGNAL flag:STD_LOGIC_VECTOR(2 downto 0);
+SIGNAL flag_in:STD_LOGIC_VECTOR(2 downto 0);
+SIGNAL flag_out:STD_LOGIC_VECTOR(2 downto 0);
 
 SIGNAL e_flag:std_logic;
 SIGNAL z_flag:std_logic;
@@ -174,22 +175,22 @@ addOrSub <= busOut(7);
 	                              "ZZZZZZZZ" WHEN OTHERS; -- denna behövs nog inte
 
 
-rca_jump: entity work.rca PORT MAP(pcOut,add_inp,addOrSub,C,jumpAddr); -- vad händer med carryin?
-rca_pclncr: entity work.rca PORT MAP(pcOut,"00000001",'0',C,pclncrOut); -- vad händer med carryin?
-
+rca_jump: entity work.rca PORT MAP(pcOut,add_inp,addOrSub,open,jumpAddr); -- vad händer med carryin?
+rca_pclncr: entity work.rca PORT MAP(pcOut,"00000001",'0',open,pclncrOut); -- vad händer med carryin?
+--(1 => '1', OTHERS)
 with pcSel SELECT 
 	nextPC <= pclncrOut WHEN '0',
 		  jumpAddr  WHEN '1',
 		 "ZZZZZZZZ" WHEN OTHERS;
 
-reg_pc: entity work.reg PORT MAP(clk,'1',pcLd,nextPC,pcOut); -- vad händer restn?
+reg_pc: entity work.reg PORT MAP(clk,resetn,pcLd,nextPC,pcOut); -- vad händer restn?
 pc2seg <= pcOut; -- dessa 2steg outputs, ska vi bara inputta värden?
 
 mem_INS: entity work.memory 
-generic map (DATA_WIDTH => 8,
-             ADDR_WIDTH =>12,
+generic map (DATA_WIDTH =>12,
+             ADDR_WIDTH =>8,
              INIT_FILE => "i_memory_lab2.mif") 
-	     PORT MAP(clk,imRead,'0',pcOut,"00000000",imDataOut);
+	     PORT MAP(clk,imRead,'0',pcOut,"000000000000",imDataOut);
 
 imDataOut2seg <= imDataOut;
 opcode <= imDataOut(11 DOWNTO 8);
@@ -203,18 +204,23 @@ dmDataOut2seg <= dmDataOut;
 ALU: entity work.alu_wRCA PORT MAP(accOut,busOut,aluOp,alu_out,C,E,Z);
 aluOut2seg <= alu_out;
 
-flag <= E&C&Z WHEN flagLd = '1' ELSE "ZZZ"; -- spara E C Z som variabler? Skapar latches typ som bilden?
-e_flag <= flag(2);
-z_flag <= flag(0);
+--flag <= E&C&Z WHEN flagLd = '1' ELSE "ZZZ"; -- spara E C Z som variabler? Skapar latches typ som bilden?
+--e_flag <= flag(2);
+--z_flag <= flag(0);
+flag_in <= E&C&Z;
+
+flag_reg: entity work.reg generic map (width => 3) PORT MAP(clk,resetn,flagLd,flag_in,flag_out);
+e_flag <= flag_out(2);
+z_flag <= flag_out(0);
 
 with accSel SELECT 
 	accSel_out <= alu_out WHEN '0',
 		      busOut  WHEN '1',
 		   "ZZZZZZZZ" WHEN OTHERS;
 
-acc_reg: entity work.reg PORT MAP(clk,'1',accLd,accSel_out,accOut); -- vad händer restn?
+acc_reg: entity work.reg PORT MAP(clk,resetn,accLd,accSel_out,accOut); -- vad händer restn?
 acc2seg <= accOut;
 
-ds_reg: entity work.reg PORT MAP(clk,'1',dsLd,accOut,ds2seg); -- vad händer restn?
+ds_reg: entity work.reg PORT MAP(clk,resetn,dsLd,accOut,ds2seg); -- vad händer restn?
 
 END Dataflow;
