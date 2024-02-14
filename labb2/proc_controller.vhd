95b5ea4234b741425f54d1c7c2016fa9723ca35e
Kasper Fång Wiiik <kasperfa@student.chalmers.se>
2024-02-14 11:19:47 +0100

    kancomp

--- labb2/proc_controller.vhd
+++ labb2/proc_controller.vhd
@@ -32,12 +32,13 @@ ARCHITECTURE Behavior OF proc_controller IS
 	type state_type is (FE, DE1, DE2, EX, ME);
 	SIGNAL curr_state : state_type;
 	SIGNAL next_state : state_type;
-	SIGNAL OPout:STD_LOGIC_VECTOR(13 downto 0); -- 14 bit's 
+	SIGNAL OPout:STD_LOGIC_VECTOR(13 downto 0):= (others => '0'); -- 14 bit's, behöver start värde?
 
 BEGIN
 	FSM_FF: PROCESS(clk, resetn)
 	BEGIN 	
 		IF resetn = '0' THEN -- Asyncron reset
+		   --OPout <= (OTHERS => '0'); -- behöver?
 	   	   curr_state <= FE;
 		ELSIF (clk'EVENT AND clk = '1') THEN
 		   curr_state <= next_state;
@@ -51,17 +52,13 @@ BEGIN
 	next_state <= curr_state;
 	CASE curr_state IS
 		WHEN FE =>
+			next_state <= DE1;
+		WHEN DE1 =>
 			IF opcode = O_NOOP THEN
 			next_state <= FE;
-			ELSIF opcode < O_CMP THEN -- Behöver inte hoppa till DE1
-			next_state <= EX;
 			ELSIF opcode = O_SB THEN 
 			next_state <= ME;
-			ELSE
-			next_state <= DE1;
-			END IF;
-		WHEN DE1 =>
-			IF opcode = O_LBI THEN
+			ELSIF opcode = O_LBI THEN
 			  next_state <= DE2;
 			ELSIF opcode = O_SBI THEN
 			  next_state <= ME;
@@ -85,51 +82,56 @@ BEGIN
 	BEGIN
 	CASE next_state IS
 		WHEN FE => 
-			  OPout(13 DOWNTO 7) <= "0--0---";
+			  OPout(13 DOWNTO 7) <= "0000000";--"0--0---";
 			  OPout(6 DOWNTO 0)  <= "1001000";
 		WHEN DE1 =>
+			
 			IF opcode = O_ROL THEN -- behövs detta äns om rol inte sättser något?
-			  OPout(13 DOWNTO 7) <= "0--0000";
+			  OPout(13 DOWNTO 7) <= "0000000";--"0--0000";
 			  OPout(6 DOWNTO 0)  <= (OTHERS => '0');
-			ELSE 
-			  OPout(13 DOWNTO 7) <= "1000---";
+			ELSIF opcode = O_SB THEN
+			--inget va?
+			ELSIF opcode > O_JZ THEN
+			  OPout(13 DOWNTO 7) <= "1000000";--"1000---";
 			  OPout(6 DOWNTO 0)  <= "0100000";
+			ELSE
+			--inget va?
 			END IF;
 		WHEN DE2 => 
-			  OPout(13 DOWNTO 7) <= "10100--";
+			  OPout(13 DOWNTO 7) <= "1010000";--"10100--";
 			  OPout(6 DOWNTO 0)  <= "0100000";
 		WHEN EX =>
 			OPout(8 DOWNTO 7) <= opcode(1 DOWNTO 0);
 
 			IF opcode = O_IN THEN
-			  OPout(13 DOWNTO 7) <= "11101";
+			  OPout(13 DOWNTO 9) <= "11101";
 			  OPout(6 DOWNTO 0) <="0000010";
 			ELSIF opcode = O_DS THEN
-			  OPout(13 DOWNTO 7) <= "0--0-";
+			  OPout(13 DOWNTO 9) <= "00000";--"0--0-";
 			  OPout(6 DOWNTO 0) <="0000001";
 			ELSIF opcode = O_MOV THEN
-			  OPout(13 DOWNTO 7) <= "10001";
+			  OPout(13 DOWNTO 9) <= "10001";
 			  OPout(6 DOWNTO 0) <="0000010";
 			ELSIF((opcode = O_JE) AND (e_flag = '1')) OR
 			   (opcode = O_JE AND NOT (e_flag = '1'))OR
 			   (opcode = O_JE AND (z_flag = '1')) THEN
-				OPout(13 DOWNTO 7) <= "1001-";
+				OPout(13 DOWNTO 9) <= "10010";--"1001-";
 				OPout(6 DOWNTO 0)  <="0001000";
-			ELSIF opcode = O_ROL THEN
-			  OPout(13 DOWNTO 7) <= "0--00";
-			  OPout(6 DOWNTO 0) <="0000110";
 			ELSIF opcode = O_CMP THEN
-			  OPout(13 DOWNTO 7) <= "1010-";
+			  OPout(13 DOWNTO 9) <= "10100";--"1010-";
 			  OPout(6 DOWNTO 0) <="0000100";
+			ELSIF opcode = O_ROL THEN
+			  OPout(13 DOWNTO 9) <= "00000";--"0--00";
+			  OPout(6 DOWNTO 0) <="0000110";
 			ELSIF opcode > O_SUB THEN
-			  OPout(13 DOWNTO 7) <= "10101";
+			  OPout(13 DOWNTO 9) <= "10101";
 			  OPout(6 DOWNTO 0) <="0000010";
 			ELSE
-			  OPout(13 DOWNTO 7) <= "10100";
+			  OPout(13 DOWNTO 9) <= "10100";
 			  OPout(6 DOWNTO 0) <="0000110";
 			END IF;
 		WHEN ME =>
-			  OPout(13 DOWNTO 7) <= "1000---";
+			  OPout(13 DOWNTO 7) <= "1000000";--"1000---";
 			  OPout(6 DOWNTO 0)  <= "0001000";
 			IF opcode = O_SBI THEN
 			  OPout(11) <= '1';
@@ -149,13 +151,15 @@ BEGIN
 	IF master_load_enable = '1' THEN
 		imRead     <= OPout(6);
 		dmRead     <= OPout(5);
-		pcLd       <= OPout(4);
-		flagLd     <= OPout(3);
-		accLd      <= OPout(2);
-		dsLd       <= OPout(1);
+		dmWrite    <= OPout(4);
+		pcLd       <= OPout(3);
+		flagLd     <= OPout(2);
+		accLd      <= OPout(1);
+		dsLd       <= OPout(0);
 	ELSE
 		imRead     <= '0';
 		dmRead     <= '0';
+		dmWrite    <= '0';
 		pcLd       <= '0';
 		flagLd     <= '0';
 		accLd      <= '0';
